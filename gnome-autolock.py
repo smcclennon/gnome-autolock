ed7bd647a6ae7f2c47d52255316e56ea17417a2d
Sourcery AI <>
2022-07-13 10:35:16 +0000

    'Refactored by Sourcery'

--- gnome-autolock.py
+++ gnome-autolock.py
@@ -1,6 +1,6 @@
 # github.com/smcclennon/gnome-autolock
 import os, time, subprocess
-for i in range(0, 150):  # Try to lock 150 times
+for _ in range(150):
     time.sleep(0.1)  # 150 x 0.1 = Try to lock for 15 seconds
     os.system("gnome-screensaver-command --lock")  # Attempt to lock
     lock_status = subprocess.getoutput("gnome-screensaver-command --query")  # Get current lock status
